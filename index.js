dd0959c1709e793f67e0885608eb7127b905531a
Adam Kelly <adam.guitar12@gmail.com>
2018-08-14 11:15:02 +0100

    :recycle: Switched to ES5

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+"use strict";
+
 // This is a function for safe property access,
 // acting quite like the `get` function from lodash
 
-function get(object, key = [], defaultVal = null) {
-  const keys = Array.isArray(key) ? key : key.split('.')
-  const result = keys.reduce((res, key) => {
-    if (res && key in res) {
-      return res[key]
-    }
-    return false
-  }, object)
+function get(object) {
+  var key =
+    arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+  var defaultVal =
+    arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
 
-  return result || defaultVal
+  var keys = Array.isArray(key) ? key : key.split(".");
+  var result = keys.reduce(function(res, key) {
+    if (res && key in res) {
+      return res[key];
+    }
+    return false;
+  }, object);
+
+  return result || defaultVal;
 }
 
-module.exports = get
-
+module.exports = get;
